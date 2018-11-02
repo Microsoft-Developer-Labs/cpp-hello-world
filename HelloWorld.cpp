582e4584853f12aabfe3b192586951d04fa2c5d4
Microsoft-Developer-Labs <44707405+Microsoft-Developer-Labs@users.noreply.github.com>
2018-11-02 11:02:23 -0600

    The program file.

---
+++ HelloWorld.cpp
@@ -0,0 +1,6 @@
+using namespace std; //This is so that we wont have to use std::cout all the time.
+
+int main() //Main method to be called.
+{
+  cout << "Hello World!"; //Makes Hello World visible!
+}
